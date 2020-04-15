74b64c30ac2ee2a815882323e367f68376285318
Mark Vartanyan <kolypto@gmail.com>
2020-04-16 01:52:00 +0400

    README.md: improved wording & formatting

--- nplus1loader/__init__.py
+++ nplus1loader/__init__.py
@@ -40,22 +40,28 @@ First of all, what's the N+1 problem?
 Suppose you have the following code which loads a number of objects from the DB,
 but some fields are deferred:
 
-    users = ssn.query(User).options(
-        load_only('id')  # worst case scenario
-    )
+```python
+users = ssn.query(User).options(
+    load_only('id')  # worst case scenario
+)
+```
 
 And then somewhere in the code some `jsonify()` function decides to check one of
 the unloaded properties:
 
-    for user in users:
-        user.state  # load the attribute
+```python
+for user in users:
+    user.state  # load the attribute
+```
 
 Because the attribute hasn't been loaded on the whole collection...
 it will trigger N queries to load that attribute on every iteration of the loop.
 Crying shame.
 
 In many cases we can tell SqlAlchemy to load exactly the attributes we are going
-to use, but in other cases that's utterly impossible.
+to use (by using `undefer()` / `joinedload()` / `selectinload()`), but in other
+cases that's utterly impossible. We just don't know the names of those attributes
+in advance.
 
 The Solution
 ============
@@ -67,30 +73,36 @@ all of them for you.
 The solution is, however, not all-encompassing: you only enable it where you see fit.
 It's implemented as a load option, like this:
 
-    from nplus1loader import nplus1loader
+```python
+from nplus1loader import nplus1loader
 
-    users = ssn.query(User).options(
-        load_only('id'),  # worst case scenario
-        nplus1loader('*')  # include every column and every relationship
-    )
+users = ssn.query(User).options(
+    load_only('id'),  # worst case scenario
+    nplus1loader('*')  # include every column and every relationship
+)
+```
 
 Now, whenever you touch any unloaded column or relationship on any of those loaded users,
 the attribute will be loaded for all User instance in that session.
 So now, you can safely iterate through them.
 
-Warning: do not rely on this mechanism; only use it as a safeguard for production.
+*Warning*: do not rely on this mechanism; only use it as a safeguard for production apps.
 It is recommended that you do something like this instead:
 
-    from nplus1loader import default_columns, raiseload_all, nplus1loader
+```python
+from nplus1loader import default_columns, raiseload_all, nplus1loader
 
-    users = ssn.query(User).options(
-        default_columns(User),
-        raiseload_all('*') if in_testing else nplus1loader('*')
-    )
+users = ssn.query(User).options(
+    default_columns(User),
+    raiseload_all('*') if in_testing else nplus1loader('*')
+)
+```
 
-Make sure you import the module first in order for those options to get registered :)
+Make sure you import the module first in order for those load options to get registered :)
 
-    import nplus1loader
+```python
+import nplus1loader
+```
 
 One minor inconvenience
 =======================
@@ -98,26 +110,28 @@ One minor inconvenience
 Did you notice the `default_columns()` load option?
 You'll have to insert it early into your query, I'm sorry.
 
-This is because nplus1loader('*') is a catch-all column loader option.
+This is because `nplus1loader('*')` is a catch-all column loader option.
 In other words, if no column strategies are given by you, there are no loaded columns,
-and this nplus1loader('*') will handle all columns!
+and this `nplus1loader('*')` will handle all columns!
 
 This is not what we want. We would instead want to apply the defaults... then override them...
-and put this nplus1loader('*') in the end. But that's not possible with SqlAlchemy.
+and put this `nplus1loader('*')` in the end. But that's not possible with SqlAlchemy.
 
-So we have to live with this default_columns(Model): it reads the default loading strategies
+So we have to live with this `default_columns(Model)`: it reads the default loading strategies
 from your Model and applies it early. In other words, it knows which columns are deferred by default.
 
-If you don't put it there, nplus1loader() will remove all columns from the query, and you'll get
+If you don't put it there, `nplus1loader()` will remove all columns from the query, and you'll get
 this weird error:
 
     sqlalchemy.exc.InvalidRequestError: Query contains no columns with which to SELECT from.
 
 This is effectively the same as `defer('*')`.
 
-Now... why can't we just undefer('*') everything?
-Because that's not right with some Column(deferred=True), and Column(deferred_group='password'),
+Now... why can't we just `undefer('*')` everything?
+
+Because that's not right with some `Column(deferred=True)`, and `Column(deferred_group='password')`,
 which have other preferences about being loaded.
+So `default_columns(User)` will respect those defaults.
 
 Logging
 =======
@@ -162,21 +176,25 @@ The `nplus1loader` package has other tools that you might find useful.
 First of all, you can be more specific about which attributes you want to have handled by the N+1 loader.
 You can opt to handle only relationships, all, or specific ones:
 
-    nplus1loader_rels('*')
-    nplus1loader_rels(relationshop_name, ...)
+```python
+nplus1loader_rels('*')
+nplus1loader_rels(relationship_name, ...)
+```
 
 or you can opt to handle only columns:
 
-    nplus1loader_cols('*')
-    nplus1loader_cols(column_name, ...)
+```python
+nplus1loader_cols('*')
+nplus1loader_cols(column_name, ...)
+```
 
 `default_columns(Model)`
 ------------------------
 
-This loader option takes the default defer()/undefer() settings from a model and sets them on a Query.
+This loader option takes the default `defer()`/`undefer()` settings from a model and sets them on a Query.
 In itself, this option is useless, but it enables you to alter the defaults by using other loading options.
 
-In particular, `nplus1loader('*')` won't work unless you provide `default_columns()` first.
+In particular, `nplus1loader('*')` won't work unless you apply `default_columns()` first.
 
 
 `raiseload_all('*')`
@@ -186,11 +204,13 @@ Raiseload for both columns and relationships.
 
 Example:
 
-    ssn.query(User).options(
-        default_columns('id'),
-        # Raise an error if any attribute at all is lazy-loaded
-        raiseload_all('*')
-    )
+```python
+ssn.query(User).options(
+    default_columns('id'),
+    # Raise an error if any attribute at all is lazy-loaded
+    raiseload_all('*')
+)
+```
 
 The error that it throws is `nplus1loader.LazyLoadingAttributeError()`, so it's easy to catch in your code ;)
 
@@ -202,11 +222,13 @@ Raiseload for columns. Use to fine-tune.
 
 Example:
 
-    ssn.query(User).options(
-        load_only('id'),
-        # Raise an error if any other column is lazy-loaded
-        raiseload_col('*')
-    )
+```python
+ssn.query(User).options(
+    load_only('id'),
+    # Raise an error if any other column is lazy-loaded
+    raiseload_col('*')
+)
+```
 
 You can also specify individual columns as a list.
 
@@ -225,9 +247,14 @@ error instead.
 `bulk_load_attribute_for_instance_states()`
 -------------------------------------------
 
-This is the heart of N+1 loader. You give it a list of instances, name one unlaoded attribute,
+This is the heart of N+1 loader.
+
+You give it a list of instances, name one unlaoded attribute,
 and it makes just one query to load this attribute's value from the database, and efficiently
 augments all the existing instances with the loaded value of this attribute.
+
+This is an advanced feature.
+See the code: [nplus1loader/bulk_load.py](nplus1loader/bulk_load.py)
 """
 
 # Loader options
