2c0cb0c7acdf6283b38bbb9cec9232643da13044
Mark Vartanyan <kolypto@gmail.com>
2020-11-27 16:00:08 +0300

    Migrated to Poetry

--- noxfile.py
+++ noxfile.py
@@ -1,36 +1,32 @@
 import nox.sessions
 
 
+PYTHON_VERSIONS = ['3.6', '3.7', '3.8', '3.9']
+SQLALCHEMY_VERIONS = [
+    '1.2.19',
+    *(f'1.3.{x}'
+      for x in range(0, 1 + 20)),
+]
+
+
 nox.options.reuse_existing_virtualenvs = True
 nox.options.sessions = ['tests', 'tests_sqlalchemy']
 
 
-@nox.session(python=['3.6', '3.7', '3.8'])
+@nox.session(python=PYTHON_VERSIONS)
 def tests(session: nox.sessions.Session, sqlalchemy=None):
     """ Run the full tests suite """
+    session.install('poetry')
+    session.run('poetry', 'install')
 
-    session.install('.', '-r', 'requirements-dev.txt')
     if sqlalchemy:
         session.install(f'sqlalchemy=={sqlalchemy}')
 
-    session.run('nosetests', 'tests/')
+    session.run('pytest', 'tests/')
 
 
 @nox.session(python='3.8')
-@nox.parametrize(
-    'sqlalchemy',
-    [
-        '1.2.19',
-        *(f'1.3.{x}'
-          for x in range(0, 1+19)),
-    ]
-)
+@nox.parametrize('sqlalchemy', SQLALCHEMY_VERIONS)
 def tests_sqlalchemy(session: nox.sessions.Session, sqlalchemy):
     """ Test against a specific SqlAlchemy version """
     tests(session, sqlalchemy)
-
-
-@nox.session
-def build(session: nox.sessions.Session):
-    """ Build the package """
-    session.run('python', 'setup.py', 'build', 'sdist', 'bdist_wheel')
